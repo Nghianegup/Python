4b079dd50572be5d0d0e17dbcf402bc627d9dc02
Nghianegup <49435488+Nghianegup@users.noreply.github.com>
2019-06-18 12:03:08 +0700

    18/6

---
+++ h/notebook.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\dell_5560\Anaconda2\Scripts\activate C:\Users\dell_5560\Anaconda2
+jupyter-notebook >C:\Users\dell_5560\.anaconda\navigator\scripts\notebook-out-1.txt 2>C:\Users\dell_5560\.anaconda\navigator\scripts\notebook-err-1.txt
